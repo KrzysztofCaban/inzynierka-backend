164ea19a834e3f15fec2c1235a9b6371738e6494
Krzysztof Caban <kcaban99@gmail.com>
2022-11-10 11:51:23 +0100

    Modified LevelService

---
+++ src/main/java/acc/inzynierka/exception/level/LevelDifficultyAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package acc.inzynierka.exception.level;
+
+import acc.inzynierka.exception.ApiRuntimeException;
+
+public class LevelDifficultyAlreadyExistsException extends ApiRuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public LevelDifficultyAlreadyExistsException() {
+        super(String.format("Poziom o podanym poziomie trudności jest już w użyciu w obecnym kursie"));
+    }
+}
