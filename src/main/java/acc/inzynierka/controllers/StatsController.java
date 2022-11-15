bef6a88485b2ab40d51d70eae84ca2ed40a180f8
Paweł Czuchryta <64778637+Ybalath@users.noreply.github.com>
2022-11-15 18:16:22 +0100

    Created StatsService

--- src/main/java/acc/inzynierka/controllers/StatsController.java
+++ src/main/java/acc/inzynierka/controllers/StatsController.java
@@ -1,16 +1,11 @@
 package acc.inzynierka.controllers;
 
-import acc.inzynierka.modelsDTO.CourseStatsDto;
-import acc.inzynierka.modelsDTO.newUsersPerDay;
-import acc.inzynierka.repository.CourseRepository;
-import acc.inzynierka.utils.UserUtil;
+import acc.inzynierka.services.StatsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -19,26 +14,32 @@ import java.util.List;
 public class StatsController {
 
     @Autowired
-    CourseRepository courseRepository;
+    StatsService statsService;
 
     @GetMapping("courses")
     public ResponseEntity<?> getCoursesStats() {
-        Long adminID = UserUtil.getUser();
-
-
-        List<CourseStatsDto> courseStatDtos = courseRepository.getCoursesStats(adminID);
 
         return new ResponseEntity<>(
-                courseStatDtos,
+                statsService.getStatsForAuthor(),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("coursesforall")
+    @PreAuthorize(value = "hasRole('ROLE_SUPERADMIN')")
+    public ResponseEntity<?> getCoursesStatsForAllAuthors() {
+
+        return new ResponseEntity<>(
+                statsService.getStatsForAllAuthors(),
                 HttpStatus.OK
         );
     }
 
     @GetMapping("newusersperday/{courseId}")
     public ResponseEntity<?> getNewUsersPerDay(@PathVariable Long courseId) {
-        List<newUsersPerDay> newUsersPerDays = courseRepository.getNewUsersPerDay(courseId);
+
         return new ResponseEntity<>(
-                newUsersPerDays,
+                statsService.getNewUsersPerDay(courseId),
                 HttpStatus.OK
         );
     }
