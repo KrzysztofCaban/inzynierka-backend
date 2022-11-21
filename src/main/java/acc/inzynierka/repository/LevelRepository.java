f7bf0ac7b732c1b84c7928a73a445a566a153abf
Kacper <69423945+Kacper-adamiak@users.noreply.github.com>
2022-11-21 18:55:07 +0100

    Modified LevelRepository - added method findByName

--- src/main/java/acc/inzynierka/repository/LevelRepository.java
+++ src/main/java/acc/inzynierka/repository/LevelRepository.java
@@ -4,7 +4,11 @@ import acc.inzynierka.models.Level;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LevelRepository extends JpaRepository<Level, Long> {
+
+    Optional<Level> findByName(String name);
 
 }
