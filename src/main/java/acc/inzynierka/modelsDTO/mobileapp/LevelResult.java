d99d14200ced871b18c5b7a46b61e7069b5e84e2
Paweł Czuchryta <64778637+Ybalath@users.noreply.github.com>
2022-12-02 18:51:35 +0100

    Created MobileStatsController

---
+++ src/main/java/acc/inzynierka/modelsDTO/mobileapp/LevelResult.java
@@ -0,0 +1,10 @@
+package acc.inzynierka.modelsDTO.mobileapp;
+
+import lombok.Data;
+
+@Data
+public class LevelResult {
+    private String levelName;
+    private int levelDifficulty;
+    private int levelScore;
+}
---
+++ src/main/java/acc/inzynierka/services/mobileapp/StatsMobileService.java
@@ -0,0 +1,64 @@
+package acc.inzynierka.services.mobileapp;
+
+import acc.inzynierka.models.Result;
+import acc.inzynierka.models.User;
+import acc.inzynierka.modelsDTO.mobileapp.LevelResult;
+import acc.inzynierka.payload.response.mobile.UserCourseResults;
+import acc.inzynierka.services.webapp.UserService;
+import acc.inzynierka.utils.UserUtil;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@SuppressWarnings("UnresolvedClassReferenceRepair")
+@Service
+public class StatsMobileService {
+
+
+    private final UserService userService;
+
+    public StatsMobileService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public Object getCourseResults() {
+        Long userId = UserUtil.getUser();
+        User user = userService.findById(userId);
+
+        List<UserCourseResults> userCourseResultsList = user.getCourses()
+                .stream()
+                .map(userCourse -> {
+                    UserCourseResults userCourseResults = new UserCourseResults();
+                    userCourseResults.setCourseName(userCourse.getCourse().getName());
+                    userCourseResults.setAuthorName(userCourse.getCourse().getAuthor().getLogin());
+                    userCourseResults.setResults(
+                            userCourse.getCourse()
+                                    .getLevels()
+                                    .stream()
+                                    .map(level -> {
+                                        LevelResult levelResult = new LevelResult();
+                                        levelResult.setLevelName(level.getName());
+                                        levelResult.setLevelDifficulty(level.getDifficulty());
+                                        Result resultt = new Result();
+                                        resultt.setValue(-1);
+                                        levelResult.setLevelScore(
+                                                level.getLevelResults()
+                                                        .stream()
+                                                        .filter(
+                                                                result -> result.getUser()
+                                                                        .getId()
+                                                                        .equals(userId))
+                                                        .findFirst()
+                                                        .orElse(resultt)
+                                                        .getValue());
+                                        return levelResult;
+                                    })
+//                                    .filter(out -> out.getLevelScore() != -1)
+                                    .toList());
+                    return userCourseResults;
+                })
+                .toList();
+
+        return userCourseResultsList;
+    }
+}
