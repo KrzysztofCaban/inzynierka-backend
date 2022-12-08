d98bd21aca7ded8ad378850a1e9bf3021544518a
Krzysztof Caban <kcaban99@gmail.com>
2022-12-08 12:54:51 +0100

    Fix

--- src/main/java/acc/inzynierka/payload/request/webapp/CourseRequest.java
+++ src/main/java/acc/inzynierka/payload/request/webapp/CourseRequest.java
@@ -23,7 +23,7 @@ public class CourseRequest implements Serializable {
     @NotEmpty(message = "Nazwa nie może być pusta")
     @Size(min = 4, max = 24, message = "Długość nazwy powinna zawierać się pomiędzy 4 a 24 znaków")
     private String name;
-    @Size(min = 30, max = 240, message = "Długość opisu powinna zawierać się pomiędzy 2 a 240 znaków")
+    @Size(min = 2, max = 240, message = "Długość opisu powinna zawierać się pomiędzy 2 a 240 znaków")
     private String description;
     @NotEmpty(message = "Nazwa kategori nie może być pusta")
     private String categoryName;
