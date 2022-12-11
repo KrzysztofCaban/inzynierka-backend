74ea61d42babf3d0328b16153d3cfb5db8284ec0
Krzysztof Caban <kcaban99@gmail.com>
2022-12-11 20:21:12 +0100

    Modified regexp for password

--- src/main/java/acc/inzynierka/payload/request/PasswordChangeRequest.java
+++ src/main/java/acc/inzynierka/payload/request/PasswordChangeRequest.java
@@ -11,7 +11,7 @@ public class PasswordChangeRequest {
     @NotEmpty(message = "Stare hasło nie może być pusta")
     private String oldPassword;
     @NotEmpty(message = "Nowe hasło nie może być pusta")
-    @Pattern(regexp = "^(?=.[A-Za-z])(?=d)(?=.[@$!%#?&])[A-Za-zd@$!%*#?&]{8,}$", message = "Hasło nie spełnia wymogów")
+    @Pattern(regexp = "^(?=.[A-Za-z])(?=.\\\\d)(?=.[@$!%#?&])[A-Za-z\\\\d@$!%*#?&]{8,}$", message = "Hasło nie spełnia wymogów")
     @Size(min = 6, max = 40, message = "Długość hasła powinna wynosić pomiędzy 6 a 40 znaków")
 
     private String newPassword;
